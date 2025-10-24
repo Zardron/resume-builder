cb4b38f03b26bb5a97eeee0837eb7b36a00a079d
Zardron <zardron.pesquera1993@gmail.com>
2025-10-24 23:20:58 +0800

    finalizing the dashboard UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,34 +1,91 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import { File, PlusIcon, UploadCloudIcon, PencilIcon, TrashIcon} from "lucide-react";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="max-w-7xl px-16 py-8">
-        <span className="text-sm text-gray-900 dark:text-gray-100">
-          <span className="font-bold text-gray-900 dark:text-gray-100">
-            Welcome to your Smart Resume Builder Dashboard.
+      <div className="mx-auto px-16 py-8">
+        <div className="w-full flex items-center justify-center mb-4">
+          <span className="text-sm text-gray-900 dark:text-gray-100 text-center">
+            <span className="font-bold text-gray-900 dark:text-gray-100">
+              Welcome to your Smart Resume Builder Dashboard.
+            </span>
+            <p className="text-sm font-lighter text-gray-900 dark:text-gray-100">
+              Craft a professional, standout resume in minutes — powered by AI.
+            </p>
           </span>
-          <p className="text-sm font-lighter text-gray-900 dark:text-gray-100">
-            Craft a professional, standout resume in minutes — powered by AI.
-          </p>
-          <div className="flex gap-2 mt-4">
-            <button className="w-full bg-white dark:bg-gray-800 sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg border border-gray-300 dark:border-gray-700 hover:shadow-md hover:border-dashed transition-all duration-300 cursor-pointer">
-              <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--primary-color)] to-[var(--accent-color)] rounded-full text-white" />
-              <p className="text-xs group-hover:text-indigo-600 transition-all duration-300 mt-2">
-                Create Resume
-              </p>
-            </button>
+        </div>
+        <div className="w-full flex items-center justify-center gap-4">
+          <button className="w-full bg-white dark:bg-gray-800 sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg border border-gray-300 dark:border-gray-700 hover:shadow-md hover:border-dashed transition-all duration-300 cursor-pointer">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--primary-color)] to-[var(--accent-color)] rounded-full text-white" />
+            <p className="text-xs group-hover:text-indigo-600 transition-all duration-300 mt-2">
+              Create Resume
+            </p>
+          </button>
 
-            <button className="w-full bg-white dark:bg-gray-800 sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg border border-gray-300 dark:border-gray-700 hover:shadow-md hover:border-dashed transition-all duration-300 cursor-pointer">
-              <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--accent-color)] to-[var(--primary-color)] rounded-full text-white" />
-              <p className="text-xs group-hover:text-[var(--accent-color)] transition-all duration-300 mt-2">
-                Upload Existing
-              </p>
-            </button>
-          </div>
-        </span>
+          <button className="w-full bg-white dark:bg-gray-800 sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg border border-gray-300 dark:border-gray-700 hover:shadow-md hover:border-dashed transition-all duration-300 cursor-pointer">
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--accent-color)] to-[var(--primary-color)] rounded-full text-white" />
+            <p className="text-xs group-hover:text-[var(--accent-color)] transition-all duration-300 mt-2">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+
+        <hr className="w-full border-gray-200 dark:border-gray-700 my-4" />
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 gap-4">
+          <button className="group w-full relative h-48 flex flex-col items-center justify-center rounded-lg border border-[var(--primary-color)] dark:border-gray-700 hover:shadow-sm hover:shadow-gray-500 dark:hover:shadow-gray-700 hover:translate-x-[2px] hover:translate-y-[-2px] bg-gradient-to-r from-[var(--gradient-primary-color)] to-[var(--gradient-accent-color)] transition-all duration-300 cursor-pointer">
+            <File className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--primary-color)] to-[var(--accent-color)] rounded-full text-white border border-white" />
+            <p className="text-xs group-hover:text-[var(--primary-color)] transition-all duration-300 mt-2">
+              Zardron's Resume
+            </p>
+
+            <div className="absolute bottom-2 left-0 right-0 flex items-center justify-center flex-col">
+              <p className="text-xs">Last updated:</p>
+              <p className="text-xs font-thin">10/24/2025</p>
+            </div>
+
+            <div className="absolute top-2 right-2 flex items-center gap-1 opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <PencilIcon className="size-4 text-[var(--ai-blue)] transition-all duration-300 hover:scale-110" />
+              <TrashIcon className="size-4 text-red-500 transition-all duration-300 hover:scale-110" />
+            </div>
+          </button>
+
+          <button className="group w-full relative h-48 flex flex-col items-center justify-center rounded-lg border border-[var(--primary-color)] dark:border-gray-700 hover:shadow-sm hover:shadow-gray-500 dark:hover:shadow-gray-700 hover:translate-x-[2px] hover:translate-y-[-2px] bg-gradient-to-r from-[var(--gradient-primary-color)] to-[var(--gradient-accent-color)] transition-all duration-300 cursor-pointer">
+            <File className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--primary-color)] to-[var(--accent-color)] rounded-full text-white border border-white" />
+            <p className="text-xs group-hover:text-[var(--primary-color)] transition-all duration-300 mt-2">
+              Alaine's Resume
+            </p>
+
+            <div className="absolute bottom-2 left-0 right-0 flex items-center justify-center flex-col">
+              <p className="text-xs">Last updated:</p>
+              <p className="text-xs font-thin">10/24/2025</p>
+            </div>
+
+            <div className="absolute top-2 right-2 flex items-center gap-1 opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <PencilIcon className="size-4 text-[var(--ai-blue)] transition-all duration-300 cursor-pointer hover:scale-110" />
+              <TrashIcon className="size-4 text-red-500 transition-all duration-300 cursor-pointer hover:scale-110" />
+            </div>
+          </button>
+
+          <button className="group w-full relative h-48 flex flex-col items-center justify-center rounded-lg border border-[var(--primary-color)] dark:border-gray-700 hover:shadow-sm hover:shadow-gray-500 dark:hover:shadow-gray-700 hover:translate-x-[2px] hover:translate-y-[-2px] bg-gradient-to-r from-[var(--gradient-primary-color)] to-[var(--gradient-accent-color)] transition-all duration-300 cursor-pointer">
+            <File className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-r from-[var(--primary-color)] to-[var(--accent-color)] rounded-full text-white border border-white" />
+            <p className="text-xs group-hover:text-[var(--primary-color)] transition-all duration-300 mt-2">
+              Zaine's Resume
+            </p>
+
+            <div className="absolute bottom-2 left-0 right-0 flex items-center justify-center flex-col">
+              <p className="text-xs">Last updated:</p>
+              <p className="text-xs font-thin">10/24/2025</p>
+            </div>
+
+            <div className="absolute top-2 right-2 flex items-center gap-1 opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <PencilIcon className="size-4 text-[var(--ai-blue)] transition-all duration-300 cursor-pointer hover:scale-110" />
+              <TrashIcon className="size-4 text-red-500 transition-all duration-300 cursor-pointer hover:scale-110" />
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
